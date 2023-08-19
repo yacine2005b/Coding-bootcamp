01f0426c533041226a6a2e8210432b2097d65883
yacine bouziane <yybouz1802@gmail.com>
2023-08-19 20:41:23 +0100

    update project

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const john = document.querySelector('.john');
+const tanya = document.querySelector('.tanya');
+const prev = document.querySelectorAll('.prev-btn');
+const next = document.querySelectorAll('.next-btn');
+
+john.classList.add('hide')
+const slideChange =() => {
+    if(john.classList.contains('hide')){
+    john.classList.remove('hide')
+    tanya.classList.add('hide')
+    } 
+    else if(tanya.classList.contains('hide')) {
+       tanya.classList.remove('hide');
+        john.classList.add('hide');
+    }
+    else {
+        john.classList.add('hide')
+    }
+}
+prev.forEach(item =>item.addEventListener('click',slideChange))
+next.forEach(item =>item.addEventListener('click',slideChange))
